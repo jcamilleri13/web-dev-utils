19864eb271670e8f9f2755c9cdf274423a27456e
James Camilleri <james@james.mt>
2021-09-06 10:14:06 +0200

    Add logo template

---
+++ templates/sanity/custom/styling/logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const logo = () => (
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMinYMid meet"
+    style={{ height: '3rem' }}
+    viewBox=""
+  >
+    {/* SVG code goes here. */}
+  </svg>
+)
+
+export default logo
