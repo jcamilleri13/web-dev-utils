ec0b647717d6350e91b41aa706b4f8d0ef6fc07e
James Camilleri <james@james.mt>
2021-10-23 22:18:26 +0100

    Add sanity template schema placeholders

---
+++ templates/sanity/schemas/forms/contact.js
@@ -0,0 +1,41 @@
+export default {
+  type: 'document',
+  name: 'formContact',
+  title: 'Contact Form Submission',
+
+  fields: [{
+    name: 'name',
+    title: 'Name',
+    type: 'string',
+    readonly: true
+  }, {
+    name: 'email',
+    title: 'Email',
+    type: 'string',
+    readonly: true
+  }, {
+    name: 'subject',
+    title: 'Subject',
+    type: 'string',
+    readonly: true
+  }, {
+    name: 'message',
+    title: 'Message',
+    type: 'text',
+    readonly: true
+  }],
+
+  preview: {
+    select: {
+      _createdAt: 'createdAt',
+      name: 'name',
+      subject: 'subject'
+    },
+    prepare ({ name, subject, createdAt }) {
+      return {
+        title: subject ? `${subject} (${name})` : name,
+        subtitle: (new Date(createdAt)).toLocaleString()
+      }
+    }
+  }
+}
