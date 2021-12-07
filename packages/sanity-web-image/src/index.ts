a7d669c84df52ecd99be6d83e98267a49a2b02bb
James Camilleri <james@james.mt>
2021-12-07 02:05:43 +0000

    Update WebImage type

--- packages/sanity-web-image/src/index.ts
+++ packages/sanity-web-image/src/index.ts
@@ -1,5 +1,5 @@
 export { generateImageBreakpoints } from './generate-image-breakpoints'
 export { updateImageMetadata } from './update-image-metadata'
 
-export { WebImage } from './web-image'
+export type { WebImage } from './web-image'
 export { WebImageSchema } from './sanity-schema'
