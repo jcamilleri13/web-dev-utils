b9f480acf35f7a47baeadf337097486b71906986
James Camilleri <james@james.mt>
2021-12-07 10:37:54 +0000

    Add deep-map helper

---
+++ packages/sanity-web-image/src/deep-map.ts
@@ -0,0 +1,42 @@
+export async function asyncDeepMap(
+  input: any,
+  fn: (input: any) => Promise<undefined | any>
+): Promise<any> {
+  const result = await fn(input)
+  if (result !== undefined) return result
+
+  if (Array.isArray(input)) {
+    return await Promise.all(input.map((item) => asyncDeepMap(item, fn)))
+  }
+
+  if (typeof input === 'object') {
+    return (
+      await Promise.all(
+        Object.entries(input).map(async ([key, value]) => ({
+          [key]: await asyncDeepMap(value, fn)
+        }))
+      )
+    ).reduce((combinedObject, object) => ({ ...combinedObject, ...object }), {})
+  }
+
+  return input
+}
+
+export function deepMap(input: any, fn: (input: any) => undefined | any): any {
+  const result = fn(input)
+  if (result !== undefined) return result
+
+  if (Array.isArray(input)) {
+    return input.map((item) => asyncDeepMap(item, fn))
+  }
+
+  if (typeof input === 'object') {
+    return Object.entries(input)
+      .map(async ([key, value]) => ({
+        [key]: await asyncDeepMap(value, fn)
+      }))
+      .reduce((combinedObject, object) => ({ ...combinedObject, ...object }), {})
+  }
+
+  return input
+}
