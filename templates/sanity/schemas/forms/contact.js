7b4d7d94079983d71cdd4dc91bb9d48499150143
James Camilleri <james@james.mt>
2021-12-28 18:15:31 +0100

    Make contact form submissions read-only

--- templates/sanity/schemas/forms/contact.js
+++ templates/sanity/schemas/forms/contact.js
@@ -2,28 +2,34 @@ export default {
   type: 'document',
   name: 'formContact',
   title: 'Contact Form Submission',
+  __experimental_actions: [],
 
-  fields: [{
-    name: 'name',
-    title: 'Name',
-    type: 'string',
-    readonly: true
-  }, {
-    name: 'email',
-    title: 'Email',
-    type: 'string',
-    readonly: true
-  }, {
-    name: 'subject',
-    title: 'Subject',
-    type: 'string',
-    readonly: true
-  }, {
-    name: 'message',
-    title: 'Message',
-    type: 'text',
-    readonly: true
-  }],
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      readonly: true
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+      readonly: true
+    },
+    {
+      name: 'subject',
+      title: 'Subject',
+      type: 'string',
+      readonly: true
+    },
+    {
+      name: 'message',
+      title: 'Message',
+      type: 'text',
+      readonly: true
+    }
+  ],
 
   preview: {
     select: {
@@ -31,10 +37,10 @@ export default {
       name: 'name',
       subject: 'subject'
     },
-    prepare ({ name, subject, createdAt }) {
+    prepare({ name, subject, createdAt }) {
       return {
         title: subject ? `${subject} (${name})` : name,
-        subtitle: (new Date(createdAt)).toLocaleString()
+        subtitle: new Date(createdAt).toLocaleString()
       }
     }
   }
