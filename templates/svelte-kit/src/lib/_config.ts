cf7931c947a09583aa77a10ebaae821867098883
James Camilleri <james@james.mt>
2021-12-28 19:04:56 +0100

    Add default config

---
+++ templates/svelte-kit/src/lib/_config.ts
@@ -0,0 +1,19 @@
+export default {
+
+  SVELTE: {
+    siteTitle: 'Site Name'
+  },
+
+  SANITY: {
+    projectId: '',
+    dataset: 'production',
+    apiVersion: ''
+  },
+
+  CONTACT_EMAIL: {
+    destination: 'info@__________.___',
+    host: 'mail.__________.___',
+    port: 465
+  }
+
+} as const
