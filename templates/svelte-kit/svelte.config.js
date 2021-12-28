a4d3ca25437d795d55ad381fa688e7fa91e111ad
James Camilleri <james@james.mt>
2021-12-28 18:59:58 +0100

    Update sveltekit configs

--- templates/svelte-kit/svelte.config.js
+++ templates/svelte-kit/svelte.config.js
@@ -1,24 +1,45 @@
+import path from 'path'
+
 import svg from '@poppanator/sveltekit-svg'
 import adapter from '@sveltejs/adapter-netlify'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({
+    postcss: true
+  }),
 
-	kit: {
-		adapter: adapter(),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			plugins: [svg()],
-			optimizeDeps: {
-				include: []
-			}
-		}
-	}
-};
+  kit: {
+    adapter: adapter(),
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    vite: {
+      plugins: [svg({
+        svgoOptions: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                },
+              },
+            }],
+          }
+      })],
+      resolve: {
+        alias: {
+          $assets: path.resolve('./src/assets')
+        }
+      },
+      optimizeDeps: {
+        include: []
+      }
+    }
+  }
+}
 
 export default config;
