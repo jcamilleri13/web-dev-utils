6907971126eb98e850b485b218f006d56b272bd7
James Camilleri <james@james.mt>
2021-12-29 17:52:10 +0100

    Add prefetchImageMetadata to index

--- packages/sanity-web-image/src/index.ts
+++ packages/sanity-web-image/src/index.ts
@@ -1,6 +1,7 @@
 export { optimiseAllImages } from './back-end/optimise-all-images'
 export { optimiseImage } from './back-end/optimise-image'
 export { updateImageMetadata } from './back-end/update-image-metadata'
+export { prefetchImageMetadata } from './front-end/prefetch-image-metadata'
 
 export type { WebImage } from './types/web-image'
 
