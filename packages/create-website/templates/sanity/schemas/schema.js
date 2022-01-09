dce6f73b1c8d5091c0474d822af46529646281b6
James Camilleri <james@james.mt>
2022-01-09 20:10:21 +0000

    Update Sanity template

--- packages/create-website/templates/sanity/schemas/schema.js
+++ packages/create-website/templates/sanity/schemas/schema.js
@@ -1,4 +1,4 @@
-import { WebImageSchema as WebImage } from '@jcamilleri13/sanity-web-image'
+import { WebImageSchema as WebImage } from '@james-camilleri/sanity-web-image'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import createSchema from 'part:@sanity/base/schema-creator'
 
@@ -7,9 +7,5 @@ import PagePlaceholder from './pages/placeholder'
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([
-    FormContact,
-    PagePlaceholder,
-    WebImage
-  ])
+  types: schemaTypes.concat([FormContact, PagePlaceholder, WebImage]),
 })
