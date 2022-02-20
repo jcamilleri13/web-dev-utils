6865159b930b770046aae3d2cedc874497789429
James Camilleri <james@james.mt>
2022-02-20 16:40:02 +0000

    Adjust root layout and styles

--- packages/create-website/templates/svelte-kit/src/routes/__layout.svelte
+++ packages/create-website/templates/svelte-kit/src/routes/__layout.svelte
@@ -1,10 +1,39 @@
+<script lang="ts" context="module">
+  import type { LoadInput } from '@sveltejs/kit'
+
+  export const load = async ({ url }: LoadInput) => ({ props: { url } })
+</script>
+
 <script lang="ts">
   import '../styles/global.scss'
 
-  import Header from '$lib/components/layout/Header.svelte'
-  import Footer from '$lib/components/layout/Footer.svelte'
+  import Header from '$lib/components/global/Header.svelte'
+  import Footer from '$lib/components/global/Footer.svelte'
+  import PageTransition from '$lib/components/transition/PageTransition.svelte'
+
+  export let url: URL
 </script>
 
-<Header />
-<main><slot /></main>
-<Footer />
+<div class="grid">
+  <Header />
+  <main>
+    <PageTransition {url}>
+      <slot />
+    </PageTransition>
+  </main>
+  <Footer />
+</div>
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    height: 100vh;
+    overflow: auto;
+  }
+
+  main {
+    position: relative;
+    margin: 0 var(--gutter);
+  }
+</style>
