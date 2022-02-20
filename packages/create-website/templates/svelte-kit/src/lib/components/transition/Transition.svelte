097fdb0b5b39ea0d7857119ac22dd3399fdd1806
James Camilleri <james@james.mt>
2022-02-20 16:37:45 +0000

    Add transition components to SvelteKit template

---
+++ packages/create-website/templates/svelte-kit/src/lib/components/transition/Transition.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition'
+
+  export let order = 0
+
+  const duration = 500
+  const delay = 200 * order
+</script>
+
+<div in:fade={{ duration, delay }}>
+  <slot />
+</div>
