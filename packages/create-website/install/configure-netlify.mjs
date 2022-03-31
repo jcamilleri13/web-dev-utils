ddab993589af0a832452187ffcd2ad5ac7bc6c45
James Camilleri <james@james.mt>
2022-03-31 17:41:27 +0100

    Install netlify-cli if not available

--- packages/create-website/install/configure-netlify.mjs
+++ packages/create-website/install/configure-netlify.mjs
@@ -1,6 +1,8 @@
 import { crossPlatform, spawn } from '../utils/process.mjs'
 
 export async function configureNetlify(config) {
+  // TODO add some try catch to avoid doing this unnecessarily.
+  await spawn(crossPlatform('npm'), ['install', '--global', 'netlify-cli'])
   await spawn(crossPlatform('netlify'), ['switch'])
 
   for (const template of config) {
