b1daae997eea7d939c48498be5007b03aecb6e2c
James Camilleri <james@james.mt>
2022-03-31 18:49:46 +0100

    Use new form utils for contact form

--- packages/create-website/templates/svelte-kit/src/routes/contact/index.svelte
+++ packages/create-website/templates/svelte-kit/src/routes/contact/index.svelte
@@ -17,17 +17,15 @@
   import { tick } from 'svelte'
 
   import Grid from '$lib/components/layout/Grid.svelte'
-  import Input from '$lib/components/form/Input.svelte'
   import StateButton, {
     State,
     STATE,
   } from '$lib/components/form/StateButton.svelte'
   import Title from '$lib/components/global/Title.svelte'
   import Transition from '$lib/components/transition/Transition.svelte'
+  import Fields from '$lib/components/form/Fields.svelte'
+  import { EMAIL_REGEX } from '$lib/utils/form'
 
-  // Taken from https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression.
-  const EMAIL_REGEX =
-    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
   const RESET_TIMEOUT = 5000
 
   let form: HTMLFormElement
@@ -37,6 +35,7 @@
   let fields = [
     {
       name: 'name',
+      type: 'text',
       value: '',
       valid: undefined,
     },
@@ -51,6 +50,7 @@
     },
     {
       name: 'subject',
+      type: 'text',
       value: '',
       valid: undefined,
     },
@@ -97,14 +97,7 @@
   <Transition order={0}>
     <form bind:this={form} name="contact">
       <Grid>
-        {#each fields as field}
-          <Input
-            {...field}
-            validate={submitted}
-            bind:value={field.value}
-            bind:valid={field.valid}
-          />
-        {/each}
+        <Fields fieldInfo={fields} validate={submitted} />
         <StateButton
           on:click={submit}
           state={formState}
