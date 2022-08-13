1066614d595663a84805ed267b76f0a6f020603d
James Camilleri <james@james.mt>
2022-08-13 16:50:58 +0100

    Update SvelteKit config

--- packages/create-website/templates/svelte-kit/svelte.config.js
+++ packages/create-website/templates/svelte-kit/svelte.config.js
@@ -1,19 +1,5 @@
-import path from 'path'
-
-import svg from '@poppanator/sveltekit-svg'
-import replace from '@rollup/plugin-replace'
 import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
-
-import CONFIG from './src/config.js'
-
-const BREAKPOINT_STRINGS = Object.entries(CONFIG.BREAKPOINTS).reduce(
-  (replaceConfig, [breakpoint, width]) => ({
-    ...replaceConfig,
-    [`__breakpoint-${breakpoint}__`]: width,
-  }),
-  {},
-)
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -23,33 +9,8 @@ const config = {
 
   kit: {
     adapter: adapter(),
-    vite: {
-      plugins: [
-        replace({
-          values: BREAKPOINT_STRINGS,
-          preventAssignment: true,
-        }),
-        svg({
-          svgoOptions: {
-            plugins: [
-              {
-                name: 'preset-default',
-                params: {
-                  overrides: {
-                    removeTitle: false,
-                    removeViewBox: false,
-                  },
-                },
-              },
-            ],
-          },
-        }),
-      ],
-      resolve: {
-        alias: {
-          $assets: path.resolve('./src/assets'),
-        },
-      },
+    alias: {
+      $assets: 'src/assets',
     },
   },
 }
