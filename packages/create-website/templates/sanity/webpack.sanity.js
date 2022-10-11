161748df0fd2cfe145233f1893358067ce2c7d77
James Camilleri <james@james.mt>
2022-10-11 22:38:19 +0100

    Update environment configuration

--- packages/create-website/templates/sanity/webpack.sanity.js
+++
@@ -1,7 +0,0 @@
-// Stop Sanity blowing up with ES6+ packages in the `@james-camilleri` namespace.
-module.exports = function (config, options) {
-  config.module.rules[0].exclude =
-    /(node_modules|bower_components)\/(?!@james-camilleri)/
-
-  return config
-}
