8870f6390e6763176e42896f8854cb40789819e8
James Camilleri <james@james.mt>
2023-02-13 22:39:46 +0000

    Add global instance of logger to exports

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,3 +1,6 @@
-export { Logger } from './logger.js'
+import { Logger } from './logger.js'
+
 export { LOG_LEVEL } from './types.js'
 export * from './plugins/index.js'
+export { Logger }
+export const log = new Logger()
