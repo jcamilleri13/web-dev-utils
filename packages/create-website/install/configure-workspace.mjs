b10186231ed9c32da865c8c67f3f2d967299eaef
James Camilleri <james@james.mt>
2023-02-24 01:00:33 +0000

    Add gitignore to monorepo

--- packages/create-website/install/configure-workspace.mjs
+++ packages/create-website/install/configure-workspace.mjs
@@ -1,7 +1,9 @@
 import { promises as fs } from 'fs'
 
 export async function configureWorkspace(cwd) {
-  const workspace = `packages:\n  - 'sites/*'`
+  const workspace = `packages:\n  - 'sites/*'\n\n`
+  const gitignore = 'node_modules\n\n'
 
   await fs.writeFile(`${cwd}/pnpm-workspace.yaml`, workspace)
+  await fs.writeFile(`${cwd}/.gitignore`, gitignore)
 }
