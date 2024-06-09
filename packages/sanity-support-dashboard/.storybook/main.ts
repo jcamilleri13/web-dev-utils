e1ed39f44cfe6923a68207924ced8952087aecba
James Camilleri <james@james.mt>
2024-06-09 17:37:42 +0100

    Configure Storybook

---
+++ packages/sanity-support-dashboard/.storybook/main.ts
@@ -0,0 +1,25 @@
+// Based on https://github.com/sanity-io/ui Storybook config.
+
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-a11y',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-links',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-themes',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+}
+
+export default config
---
+++ packages/sanity-support-dashboard/.storybook/preview.tsx
@@ -0,0 +1,67 @@
+// Based on https://github.com/sanity-io/ui Storybook config.
+
+import type { Preview } from '@storybook/react'
+import { themes } from '@storybook/theming'
+import { DecoratorHelpers } from '@storybook/addon-themes'
+import { StoryFn } from '@storybook/react'
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+import { Card, ThemeProvider, studioTheme } from '@sanity/ui'
+
+const { initializeThemeState, pluckThemeFromContext, useThemeParameters } =
+  DecoratorHelpers
+
+export const GlobalStyle = createGlobalStyle`
+  body,
+  .docs-story {
+    background-color: ${({ theme }) => theme.sanity.color.base.bg};
+  }
+`
+
+const withSanityTheme = ({ themes, defaultTheme }) => {
+  initializeThemeState(Object.keys(themes), defaultTheme)
+
+  return (Story: StoryFn, context) => {
+    const selectedTheme = pluckThemeFromContext(context)
+    const { themeOverride } = useThemeParameters()
+
+    const selected = themeOverride || selectedTheme || defaultTheme
+
+    return (
+      <ThemeProvider scheme={selected} theme={studioTheme}>
+        <GlobalStyle />
+        <Card padding={4}>
+          <Story />
+        </Card>
+      </ThemeProvider>
+    )
+  }
+}
+
+const preview: Preview = {
+  decorators: [
+    withSanityTheme({
+      themes: { light: 'light', dark: 'dark' },
+      defaultTheme: 'dark',
+    }),
+  ],
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    backgrounds: { disable: true },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    docs: {
+      theme: {
+        ...themes.dark,
+        fontBase: 'Inter, sans-serif',
+      },
+    },
+    layout: 'fullscreen',
+  },
+}
+
+export default preview
