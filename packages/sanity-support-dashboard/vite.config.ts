0bad24cdce206abd8fe6ff1fc612d0da616de85b
James Camilleri <james@james.mt>
2024-07-23 00:19:18 +0100

    Create basic build

--- packages/sanity-support-dashboard/vite.config.ts
+++ packages/sanity-support-dashboard/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      formats: ['es'],
+      fileName: 'dashboard',
+    },
+
+    // TODO: This isn't bundling correctly, the dist folder is over 4mb.
+    rollupOptions: {
+      external: [
+        '@sanity/ui',
+        'react',
+        'react/jsx-runtime',
+        'react-dom',
+        'styled-components',
+      ],
+      output: {
+        globals: {
+          react: 'react',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    },
+  },
 })
