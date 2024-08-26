8e1836e08b3c5ea2a4c6c98c742a0ef22537356f
James Camilleri <james.camilleri@fx.sgx.com>
2024-08-26 23:59:41 +0100

    Add shared package to monorepo package.json files

--- packages/create-website/install/configure-workspace.mjs
+++ packages/create-website/install/configure-workspace.mjs
@@ -30,4 +30,26 @@ end_of_line = lf`
   await fs.writeFile(`${cwd}/pnpm-workspace.yaml`, workspace)
   await fs.writeFile(`${cwd}/.gitignore`, gitignore)
   await fs.writeFile(`${cwd}/.editorconfig`, editorconfig)
+
+  // Add "shared" package to all sub-sites.
+
+  const siteDirs = (await fs.readdir(`${cwd}/sites`, { withFileTypes: true }))
+    .filter((entry) => entry.isDirectory())
+    .filter((dir) => dir.name !== '_shared')
+    .map((dir) => `${dir.parentPath}/${dir.name}`)
+
+  await Promise.all(
+    siteDirs.map(async (siteDir) => {
+      const packageJsonRaw = await fs.readFile(`${siteDir}/package.json`, 'utf-8')
+      const packageJson = JSON.parse(packageJsonRaw)
+
+      packageJson.devDependencies = { shared: 'workspace:^' }
+
+      await fs.writeFile(
+        `${siteDir}/package.json`,
+        JSON.stringify(packageJson, undefined, 2),
+        'utf-8',
+      )
+    }),
+  )
 }
