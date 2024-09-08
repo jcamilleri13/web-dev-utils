391b3b862f6f8e7b35bc011113342d8785196ac3
James Camilleri <james.camilleri@fx.sgx.com>
2024-09-09 00:17:40 +0100

    Rename TItle component to Heading

---
+++ packages/create-website/templates/svelte-kit/src/lib/components/global/Heading.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import CONFIG from '$lib/config'
+
+  interface Props {
+    title: string
+    subtitle?: string
+    hidden: boolean
+  }
+
+  let { title, subtitle, hidden }: Props = $props()
+
+  const prefix = CONFIG.GENERAL.siteTitle
+</script>
+
+<svelte:head>
+  <title>{prefix ? `${prefix} – ` : ''}{title}</title>
+</svelte:head>
+
+<h1 class:screen-reader-only={hidden}>{title}</h1>
+{#if subtitle}
+  <h2 class:screen-reader-only={hidden}>{subtitle}</h2>
+{/if}
+
+<style>
+  /* Remove default (large) spacing before subtitle. */
+  h2 {
+    margin-top: 0;
+  }
+</style>
